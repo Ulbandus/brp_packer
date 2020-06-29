1afb1aef5b0ec403e21cfb19dafd82d19e338c5b
Ulbandus <59124543+Ulbandus@users.noreply.github.com>
2020-06-29 11:51:31 +0300

    Update to 0.0.0.3

--- brp_packer.py
+++ brp_packer.py
@@ -1,98 +1,37 @@
 ﻿from shutil import make_archive, copytree, copy2, rmtree, unpack_archive
-from tkinter.messagebox import showerror, showinfo, showwarning
-from os.path import getsize, join
-from os import walk, mkdir, rename, remove
+from tkinter.messagebox import showinfo
+from os import mkdir, rename, remove, urandom
 from tkinter import Tk, Button
 from tkinter.filedialog import askopenfilename
-from random import randrange as rr
 from tkinter.ttk import Combobox
-from locale import getdefaultlocale
-from time import sleep
+from sys import platform
+from _locale import _getdefaultlocale
 
 class BRP_PACKER:
+    __slots__ = ['bootanimation', 'file_dont_selected', 'archive_maked',
+                 'select_file_text', 'make_archive_text', 'window',
+                 'type_of_archive']
+
     def __init__(self):
-        self.del_temp()
-        self.make_lang()
-        self.unpack_data()
-        if not self.check_files():
-            self.exit_of_programm()
-        self.show_FAQ()
         self.bootanimation = False
+        unpack_archive('resources.zip')
+        self.make_lang()
         self.main_window()
 
-    def unpack_data(self):
-        unpack_archive('resources.zip')
-        unpack_archive('./updaters.zip')
-        unpack_archive('./programm_resources.zip')
-        
     def make_lang(self):
-        locale_str = getdefaultlocale()[0]
-        if 'ru' in locale_str:
-            lang = 'ru'
-        elif 'eo' in locale_str:
-            lang = 'esperanto'
-        elif 'ja' in locale_str:
-            lang = 'japanese'
-        else:
-            lang = 'english'
-        if lang == 'ru':
-            self.file_dont_selected = 'Файл не выбран'
-            self.archive_maked = 'Архив создан!'
-            self.icon_ico_warning = 'Файл icon.ico не найден'
-            self.select_file_text = 'Выбрать файл'
-            self.make_archive_text = 'Создать архив'
-            self.developer_text = 'Разработчик'
-            self.lang_text = 'Язык'
-            self.resources_error_message = 'Директория Recourses повре' +\
-                'ждена, что-то может пойти не так'
-            self.updaters_error_message = 'Директория Updaters поврежден' +\
-                'а, что-то может пойти не так'
-        elif lang == 'esperanto':
-            self.file_dont_selected = 'Neniu dosiero elektita'
-            self.archive_maked = 'Arkivo kreita!'
-            self.icon_ico_warning = 'Dosiero ikono.ico ne trovita'
-            self.select_file_text = 'Elektu dosieron'
-            self.make_archive_text = 'Krei arkivon'
-            self.developer_text = 'Ellaboranto'
-            self.lang_text = 'Lingvo'
-            self.resources_error_message = 'Dosierujo pri Resursoj estas ' +\
-                'difektita, io eble malaperos'
-            self.updaters_error_message = 'Dosierujo pri Aktualigiloj korup' +\
-                'tas, ke io povus malbone funkcii'
-        elif lang == 'japanese':
-            self.file_dont_selected = 'ファイルが選択されていません'
-            self.archive_maked = 'アーカイブが作成されました！'
-            self.icon_ico_warning = 'ファイルicon.icoが見つかりません'
-            self.select_file_text = 'ファイルを選択'
-            self.make_archive_text = 'アーカイブを作成する'
-            self.developer_text = '開発者'
-            self.lang_text = '言語'
-            self.resources_error_message = 'リコースディレクトリが破損している' +\
-                'ため、問題が発生する可能性があります'
-            self.updaters_error_message = 'アップデータディレクトリが壊れているた' +\
-                'め、問題が発生する可能性があります'
-        else:
-            self.file_dont_selected = 'No file selected'
-            self.archive_maked = 'Archive created!'
-            self.icon_ico_warning = 'File icon.ico not found'
-            self.select_file_text = 'Select File'
-            self.make_archive_text = 'Create archive'
-            self.developer_text = 'Developer'
-            self.lang_text = 'Language'
-            self.resources_error_message = 'Recourses directory is damage' +\
-                'd, something may go wrong'
-            self.updaters_error_message = 'Updaters directory is corrupt' +\
-                'something might go wrong'
+        locale_str = _getdefaultlocale()[0].split('_')[0]
+        lang_pack = open('lang_pack.brp', 'r', encoding='utf-8').readlines()
+        line_index = [index for index in range(
+            0, len(lang_pack), 5) if lang_pack[index].strip() == locale_str][0]
+        self.file_dont_selected = lang_pack[line_index + 1].strip()
+        self.archive_maked = lang_pack[line_index + 2].strip()
+        self.select_file_text = lang_pack[line_index + 3].strip()
+        self.make_archive_text = lang_pack[line_index + 4].strip()
+        
 
     def del_temp(self):
-        files = ['./icon.ico', './programm_resources.zip', './updaters.zip']
-        dirs = ['./bootanimation_archive', './temp',
-                './programm_resources', './updaters', './__pycache__']
-        for file in files:
-            try:
-                remove(file)
-            except:
-                pass
+        dirs = ['./temp', './programm_resources',
+            './Splash', './Bootanimation', '__pycache__']
         for dir_ in dirs:
             try:
                 rmtree(dir_)
@@ -101,131 +40,76 @@ class BRP_PACKER:
 
     def make_bootanimation_archive(self):
         if not self.bootanimation:
-            self.showmessage('ERROR', self.file_dont_selected, 'error')
+            showinfo(title='Warning!', message=self.file_dont_selected)
             return
-        copytree('./programm_resources/', './bootanimation_archive/')
-        if self.type_of_archive.get() == 'Splash':
-            copy2(self.bootanimation, './bootanimation_archive/')
+        bootanimation_temp = self.bootanimation.split('/')[-1]
+        file_type = self.type_of_archive.get()
+        mkdir('./temp')
+        copy2(self.bootanimation, './temp')
+        copytree('./programm_resources', './bootanimation_archive')
+        if file_type == 'Splash':
+            rename(f'./temp/{bootanimation_temp}', './temp/Splash.img')
+            self.bootanimation = './temp/Splash.img'
+            copy2(self.bootanimation, './bootanimation_archive')
         else:
-            copy2(self.bootanimation, './bootanimation_archive/system/media/')
-        copytree(f'./updaters/{self.type_of_archive.get().strip()}/',
-                 './bootanimation_archive/META-INF/com/google/android/')
-        self.pack()
-        self.del_temp()
-        self.showmessage('Succes', self.archive_maked, 'info')
-
-    def pack(self):
-        make_archive(f'{self.type_of_archive.get()}TWRP_BRP_{rr(1, 1000)}',
+            rename(f'./temp/{bootanimation_temp}',
+                   './temp/bootanimation.zip')
+            self.bootanimation = './temp/bootanimation.zip'
+            copy2(self.bootanimation, './bootanimation_archive/system/media')
+        copytree(f'./{file_type}/',
+                 './bootanimation_archive/META-INF/com/google/android')
+        r = int.from_bytes(urandom(19), 'big') >> (19 * 8 - 8)
+        make_archive(f'{file_type}TWRP_BRP_{r}',
                      'zip',
                      './bootanimation_archive')
+        rmtree('./Splash')
+        rmtree('./Bootanimation')
+        rmtree('./bootanimation_archive')
+        self.del_temp()
+        self.bootanimation = False
+        showinfo(title='Succes', message=self.archive_maked)
+        unpack_archive('resources.zip')
 
     def main_window(self):
         self.window = Tk()
-        self.window.protocol('WM_DELETE_WINDOW', self.exit_of_programm)
-        self.window.title('BRP PACKER | by @Ulbandus')
-        self.window.geometry('304x27')
-        self.window.tkraise()
-        try:
-            self.window.iconbitmap('icon.ico')
-        except:
-            showerror("Warning!!!", self.icon_ico_warning, 'warning')
-        self.window.grab_set()
-        self.window.resizable(False, False)
+        self.window.title('BRP PACKER | @Ulbandus')
+        self.window.protocol('WM_DELETE_WINDOW', self.exit_of_programm)        
         self.type_of_archive = Combobox(self.window,
                                         values=['Bootanimation', 'Splash'],
                                         state="readonly")
-        self.type_of_archive.current(1)
-        self.type_of_archive.grid(column=2, row=0)
-        self.select_file_button = Button(
+        select_file_button = Button(
             self.window, text=self.select_file_text,
             background='black',
-            pady='2',
-            width='12', fg='white',
+            fg='white',
             command=self.get_file)
-        self.select_file_button.grid(column=1, row=0)
-        self.select_file_button = Button(
+        make_archive_button = Button(
             self.window,
             text=self.make_archive_text,
             background='black',
-            pady='2',
-            width='12',
             fg='white',
             command=self.make_bootanimation_archive)
-        self.select_file_button.grid(column=0, row=0)
+        self.type_of_archive.current(0)
+        self.type_of_archive.grid(column=2, row=0)   
+        select_file_button.grid(column=1, row=0)
+        make_archive_button.grid(column=0, row=0)
         self.window.mainloop()
 
     def get_file(self):
-        window = Tk().withdraw()
-        self.window.iconify()
         self.bootanimation = askopenfilename()
-        self.window.deiconify()
-        mkdir('./temp')
-        copy2(self.bootanimation, './temp/')
-        self.bootanimation_temp = self.bootanimation.split('/')[-1]
-        if self.type_of_archive.get() == 'Splash':
-            rename('./temp/' + self.bootanimation_temp, './temp/Splash.img')
-            self.bootanimation = './temp/Splash.img'
-        else:
-            rename('./temp/' + self.bootanimation_temp,
-                   './temp/bootanimation.zip')
-            self.bootanimation = './temp/bootanimation.zip'
-
-    def show_FAQ(self):
-        self.showmessage(
-            'ABOUT',
-            f'{self.developer_text}: Ulbandus\n{self.lang_text}: Python3',
-            'info')
-
-    def showmessage(self, title, message, msg_type):
-        window = Tk()
-        window.withdraw()
-        if msg_type == 'error':
-            showerror(title=title, message=message)
-        elif msg_type == 'info':
-            showinfo(title=title, message=message)
-        elif msg_type == 'warning':
-            showwarning(title=title, message=message)
-        else:
-            showerror(
-                title='UNKOWN ERROR',
-                message=f'{message}, {title}, ERROR')
-
-    def get_size(self, directory):
-        total_size = 0
-        for dirpath, dirnames, filenames in walk(directory):
-            for file in filenames:
-                filepath = join(dirpath, file)
-                total_size += getsize(filepath)
-        return total_size
-
-    def check_files(self):
-        if not self.get_size('./resources/') == 0:
-            self.showmessage(
-                'Recourses ERROR',
-                self.resources_error_message,
-                'warning')
-            return False
-        if not str(self.get_size('./updaters/'))[:3] == '725':
-            self.showmessage(
-                'Updaters ERROR',
-                self.updaters_error_message,
-                'warning')
-            return False
-        return True
+        if not self.bootanimation:
+            return
 
     def exit_of_programm(self):
-        try:
-            self.window.iconify()
-        except:
-            pass
-        sleep(0.3)
         self.del_temp()
         try:
+            remove('./icon.ico')
+            remove('./lang_pack.brp')
+        except:
+            pass        
+        try:
             self.window.destroy()
-            self.window.quit()
         except:
             pass
         exit()
-
 
 BRP_PACKER()
